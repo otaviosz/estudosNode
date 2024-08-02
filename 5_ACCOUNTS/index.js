4382965e71ba21ff687af350c55060dfd254cc86
Otávio <otaviodiascc@gmail.com>
2024-08-02 18:04:26 -0300

    setup inicial

---
+++ 5_ACCOUNTS/index.js
@@ -0,0 +1,10 @@
+// modulos externos
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+
+
+// modulos internos
+const fs = require("fs");
+
+
+console.log('Iniciamos Accounts')
